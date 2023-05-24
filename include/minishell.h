3210627725ac4b55a12656831b6674c06d840e2e
Lisa <lduheron@e3r8p4.42.fr>
2023-05-24 17:12:57 +0200

    Adding the different typedef for the AST

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: lduheron <lduheron@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 19:33:34 by lduheron          #+#    #+#             */
-/*   Updated: 2023/05/18 15:22:40 by lduheron         ###   ########.fr       */
+/*   Updated: 2023/05/24 17:11:31 by lduheron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,11 @@
 # include <limits.h>
 # include <stdint.h>
 
-////////////////////// ENUM //////////////////////
+//////////////////////////////////////////////////////////////////
+//																//
+//								ENUM							//
+//																//
+//////////////////////////////////////////////////////////////////
 
 enum e_type_exec
 {
@@ -45,29 +49,63 @@ enum e_type_token
 	OR_SIGN
 } ;
 
-////////////////////// STRUCTURES //////////////////////
+//////////////////////////////////////////////////////////////////
+//																//
+//							STRUCTURES							//
+//																//
+//////////////////////////////////////////////////////////////////
 
-typedef struct s_data {
+typedef struct s_data				t_data;
+typedef struct s_tokens				t_tokens;
+typedef struct s_tree				t_tree;
+typedef union u_tree_node_content	u_tree_node_content;
+typedef struct s_leaf				t_leaf;
+typedef struct s_branch				t_branch;
+
+struct s_data {
 	char	*line;
 	int		pos;
 	int		len;
-}			t_data;
+};
 
-typedef struct s_tokens
-{
+struct s_tokens {
 	enum e_type_token	type;
 	int					len;
 	char				*content;
 	struct s_tokens		*next;
-}	t_tokens;
+};
 
-typedef struct s_tree
+// The tree can contain two types of content : either it represents a simple
+// command, in which case the active value is a t_leaf structure. The concept
+// of a leaf signifies the end of a branch. Alternatively, it can represent
+// a node, in which case it needs to be split into two branches : 
+// the left branch and the right branch.
+
+union u_tree_node_content
 {
-	char	type;
-	char	content;
-}	t_tree;
+	struct t_leaf		leaf;
+	struct t_branch		node;
+}	t_tree_node_content;
 
-////////// EXEC STRUCTURES //////////////////
+struct	s_tree {
+	t_tree_node_content	content;
+	enum e_type_exec	type;
+};
+
+struct s_branch {
+	struct s_tree	*left;
+	struct s_tree	*right;
+};
+
+struct s_leaf {
+	char	*arg;
+};
+
+//////////////////////////////////////////////////////////////////
+//																//
+//						EXEC STRUCTURES							//
+//																//
+//////////////////////////////////////////////////////////////////
 
 typedef struct s_cmd
 {
@@ -92,12 +130,23 @@ typedef struct s_pipe
 // 	int		fd;
 // }	t_redirections;
 
-////////////////////// PROTOTYPES //////////////////////
+//////////////////////////////////////////////////////////////////
+//																//
+//																//
+//							PROTOTYPES 							//
+//																//
+//																//
+//////////////////////////////////////////////////////////////////
 
 // Main.c
 int			main(int argc, char **argv);
 
-////////////////////// IN AST UTILS DIR //////////////////////
+//////////////////////////////////////////////////////////////////
+//																//
+//                 	  	IN AST UTILS DIR   	                    //
+//																//
+//////////////////////////////////////////////////////////////////
+
 // Ft_split.c
 char		**ft_split(char const *s, char c);
 
@@ -121,7 +170,12 @@ int			ft_strncmp(const char *s1, const char *s2, size_t n);
 // Utils.c
 int			ft_strlen(char *str);
 
-////////////////////// IN LEXING DIR //////////////////////
+//////////////////////////////////////////////////////////////////
+//																//
+//                   	  IN LEXING DIR              	        //
+//																//
+//////////////////////////////////////////////////////////////////
+
 // Lexing.c
 t_tokens	*add_new_token(char *content, int type);
 int			find_type(t_data **data);
@@ -147,7 +201,12 @@ int			is_quote(char c);
 int			single_quote_management(t_data *data);
 int			double_quote_management(t_data *data);
 
-////////////////////// IN LIST UTILS DIR //////////////////////
+//////////////////////////////////////////////////////////////////
+//																//
+//               	  	IN LIST UTILS DIR   	                //
+//																//
+//////////////////////////////////////////////////////////////////
+
 // List_utils.c
 void		ft_lstadd_back(t_tokens **lst, t_tokens *new);
 void		ft_lstadd_front(t_tokens **lst, t_tokens *new);
@@ -155,13 +214,24 @@ int			ft_lstsize(t_tokens *lst);
 t_tokens	*ft_lstnew(char *content);
 t_tokens	*ft_lstlast(t_tokens *lst);
 
-////////////////////// IN PARSING DIR //////////////////////
+//////////////////////////////////////////////////////////////////
+//																//
+//                 		IN PARSING DIR   		                //
+//																//
+//////////////////////////////////////////////////////////////////
+
 // Parsing.c
 void		eat_token(t_tokens **tokens);
 void		build_tree(t_tokens **token, t_tree **tree);
 void		parse_word(t_tokens **token, t_tree **tree);
 
-////////////////////// IN STRUCTURES MANAGEMENT DIR //////////////////////
+
+//////////////////////////////////////////////////////////////////
+//																//
+//              	IN STRUCTURES MANAGEMENT DIR   	            //
+//																//
+//////////////////////////////////////////////////////////////////
+
 // Init_structures.c 
 void		init_data_structure(t_data *data, char **argv);
 void		init_token_structure(t_tokens **tokens);
